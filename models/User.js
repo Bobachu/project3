25fc471bfc89dda978c8edabe5068710f0dec125
scc85 <sarahchristinec85@gmail.com>
2019-03-20 13:32:59 -0700

    user

---
+++ models/User.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is required"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is required",
+        validate: [
+            function(input){
+                return input.length >= 6;
+            },
+            "Password should be longer"
+        ]
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
