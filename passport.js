a5bac9588784f9aac6e458ea36ce92e3919311c5
Paul Rodriguez <prodcapital@yahoo.com>
2019-03-22 10:46:32 -0700

    fridayam

--- passport.js
+++ passport.js
@@ -2,23 +2,23 @@
 
 // npm install passport passport-local mongoose passport-local-mongoose --save
 
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const passportLocalMongoose = require('passport-local-mongoose');
  
-const User = new Schema({});
+// const User = new Schema({});
  
-User.plugin(passportLocalMongoose);
+// User.plugin(passportLocalMongoose);
  
-module.exports = mongoose.model('User', User);
+// module.exports = mongoose.model('User', User);
 
-// requires the model with Passport-Local Mongoose plugged in
-const User = require('./models/user');
+// // requires the model with Passport-Local Mongoose plugged in
+// const User = require('./models/user');
  
-// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
-passport.use(User.createStrategy());
+// // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+// passport.use(User.createStrategy());
  
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 
