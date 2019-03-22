ce6f5646d367ca55e0d805614a990d5dce0e56cb
Tiffany Yin <ncarman@servpronet.com>
2019-03-22 15:17:02 -0500

    tidy api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-<<<<<<< HEAD
+
 const gamesURL = {
    url: "https://api-v3.igdb.com/games/",
    method: "GET",
@@ -9,6 +9,12 @@ const gamesURL = {
    },
 };
 
+const client_id = "jbeeh3jlwslrdqeq5reklagles1u78";
+const twitch = axios.create({
+  // baseURL: "https://api.twitch.tv/helix/",
+  accept: "application/vnd.twitchtv.v5+json",
+  headers: {"Client-ID": client_id}
+});
 
 export default {
     // Gets all users
@@ -46,24 +52,14 @@ export default {
     // Gets game info (**Have not added params yet**)
     getGameInfo: function (gameTitle) {
         return axios.get(gamesURL);
-    }
-};
-=======
+    },
 
-// 
-const client_id = "jbeeh3jlwslrdqeq5reklagles1u78";
-const twitch = axios.create({
-  // baseURL: "https://api.twitch.tv/helix/",
-  accept: "application/vnd.twitchtv.v5+json",
-  headers: {"Client-ID": client_id}
-});
-
-export default {
-  getStreamer: function(name){
+    getStreamer: function(name){
     return twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=final+fantasy+xiv");
+    
+    }
 
 };
 
 // age ratings
 // https://api-v3.igdb.com/age_ratings
->>>>>>> 34bf181c1ec6a5743c3384a7c8b279a3e40279cd
