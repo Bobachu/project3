22148c379c2044d50f2ce440d76f23f5b1c565aa
scc85 <sarahchristinec85@gmail.com>
2019-03-22 14:39:53 -0700

    update: twitch/imdb api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+const axios = require("axios");
 
-// 
+// // TWitch API credentials
 const client_id = "jbeeh3jlwslrdqeq5reklagles1u78";
 const twitch = axios.create({
   // baseURL: "https://api.twitch.tv/helix/",
@@ -8,11 +8,25 @@ const twitch = axios.create({
   headers: {"Client-ID": client_id}
 });
 
-export default {
-  getStreamer: function(name){
-    return twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=final+fantasy+xiv");
+// twitch video api 
+twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=final+fantasy+xiv").then(function (res) {
+  // console.log(data);
+  for (i = 0; i < res.data.streams.length; i++){
+    console.log(res.data.streams[i].channel.url);
+    }
+});
 
-};
-
-// age ratings
-// https://api-v3.igdb.com/age_ratings
+// IGDB API credentials
+const user_key = "940917f24ab11ddaece60ec17ad01354";
+const IDBM = axios.create({
+  accept: "application/vnd.twitchtv.v5+json",
+  headers: {"User-Key": user_key}
+});
+// IDBM rating api
+IDBM.get("https://api-v3.igdb.com/age_ratings/?fields=*").then(function(res){
+  console.log(res.data[0].rating);
+});
+// // IDBM game name/summary api
+// IDBM.get("https://api-v3.igdb.com/games/?search=Halo&fields=name,summary").then(data=>{
+//   console.log(data);
+// });
