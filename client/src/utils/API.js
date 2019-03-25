b3f5e9d345268cb67b023edeb657a03655fbf48b
scc85 <sarahchristinec85@gmail.com>
2019-03-25 13:49:36 -0700

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,7 +9,7 @@ const twitch = axios.create({
 });
 
 // twitch video api 
-twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=final+fantasy+xiv").then(function (res) {
+twitch.get("https://api.twitch.tv/kraken/search/streams?limit=4&query=final+fantasy+xiv").then(function (res) {
   // console.log(data);
   for (i = 0; i < res.data.streams.length; i++){
     console.log(res.data.streams[i].channel.url);
@@ -19,14 +19,15 @@ twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=final+fant
 // IGDB API credentials
 const user_key = "940917f24ab11ddaece60ec17ad01354";
 const IDBM = axios.create({
-  accept: "application/vnd.twitchtv.v5+json",
+  accept: "application/json",
   headers: {"User-Key": user_key}
 });
 // IDBM rating api
 IDBM.get("https://api-v3.igdb.com/age_ratings/?fields=*").then(function(res){
   console.log(res.data[0].rating);
 });
-// // IDBM game name/summary api
-// IDBM.get("https://api-v3.igdb.com/games/?search=Halo&fields=name,summary").then(data=>{
-//   console.log(data);
-// });
+// IDBM game name/summary api
+IDBM.get("https://api-v3.igdb.com/games/?search=final+fantasy&fields=name,summary").then(function(res){
+  console.log(res.data[0]);
+});
+
