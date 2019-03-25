c177239b029d2d3f9894a6c5f2aa92b0ea4f4089
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-03-25 14:04:39 -0700

    stupid footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w3-center w3-light-teal w3-padding w3-large">
+    <footer className="w3-center w3-black w3-padding w3-large">
       <p>
         Powered by{" "}
         <a
           href="https://www.w3schools.com/w3css/default.asp"
           title="W3.CSS"
           target="_blank"
-          className="w3-hover-text-green"
+          className="w3-hover-text-blue"
         >
           w3.css
         </a>
