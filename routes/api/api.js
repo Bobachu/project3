18f516a9eb4264f373564c65c248391aa7feed56
Paul Rodriguez <prodcapital@yahoo.com>
2019-03-27 11:15:30 -0700

    twitch-gbomb-hidden and working

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+const client_id = "jbeeh3jlwslrdqeq5reklagles1u78";
+const twitch = axios.create({
+  // baseURL: "https://api.twitch.tv/helix/",
+  accept: "application/vnd.twitchtv.v5+json",
+  headers: { "Client-ID": client_id }
+});
+
 // Matches with "/api/wishlists"
 router.route("/giantbomb")
   .get((req, res)=>{
@@ -16,7 +23,7 @@ router.route("/giantbomb")
   router.route("/twitch")
   .get((req, res)=>{
       console.log("twitch")
-    axios.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=assassin+creed+odyssey").then((data)=>{
+    twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=assassin+creed+odyssey").then((data)=>{
         console.log(data.data)
         res.json(data.data);
     }).catch(e=>{
@@ -25,3 +32,55 @@ router.route("/giantbomb")
   });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+// ######### DEAD CODE - FOR REFERENCE #################
+/*
+// // TWitch API credentials
+const gamesURL = {
+  url: "https://api-v3.igdb.com/games/",
+  method: "GET",
+  json: true,
+  headers: {
+    "user-key": "940917f24ab11ddaece60ec17ad01354"
+  },
+};
+const client_id = "jbeeh3jlwslrdqeq5reklagles1u78";
+const twitch = axios.create({
+  // baseURL: "https://api.twitch.tv/helix/",
+  accept: "application/vnd.twitchtv.v5+json",
+  headers: { "Client-ID": client_id }
+});
+
+// twitch video api 
+// twitch.get("https://api.twitch.tv/kraken/search/streams?limit=3&query=assassin+creed+odyssey").then(function (res) {
+//   // console.log(data);
+//   for (let i = 0; i < res.data.streams.length; i++) {
+//     console.log(res.data.streams[i].channel.url);
+//   }
+// });
+
+// Giant Bomb API credentials
+const api_key = "a9a2426d7b1ed60e55fb95133e680218e2aa7a7c";
+const giant_bomb = axios.create({
+  format: "jsonp",
+  callbackKey: "json_callback",
+  headers: { "API-Key": api_key,
+  "Access-Control-Allow-Origin": null}
+});
+
+// giant_bomb.get("https://www.giantbomb.com/api/games/?api_key=8c577a9853e23bc1a4f713365d609f1cd88126c5&format=json&filter=name:" + search + "&field_list=image,original_game_rating,deck").then(function (res) {
+//   console.log(res.data.results[0].deck);
+//   console.log(res.data.results[0].image.original_url);
+//   console.log(res.data.results[0].original_game_rating[0].name);
+// });
+*/
+
