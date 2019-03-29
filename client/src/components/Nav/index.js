903b2e95a5ebf042ed3a31dd9bb94fb54c1ccdab
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-03-29 15:07:49 -0700

    really nave bar fix

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,6 @@ import Modal from "../Modal";
 import "./nav.css";
 import Axios from "axios";
 
-
 class Nav extends Component {
   state = {
     title: "",
@@ -33,46 +32,42 @@ class Nav extends Component {
     event.preventDefault();
     if (!this.state.username) {
       alert("Fill out your username");
-    } else if (this.state.password.length > 4){
-      const {username, password} = this.state;
-      Axios.post("/api/signup", {username, password} ).then(data => {
+    } else if (this.state.password.length > 4) {
+      const { username, password } = this.state;
+      Axios.post("/api/signup", { username, password }).then(data => {
         console.log(data.data);
         this.setState({
           username: "",
           password: ""
         });
-
-
-      })
+      });
     }
   };
- 
 
   userLogin = event => {
     event.preventDefault();
     const { username, password } = this.state;
-    Axios.post("/api/login", {username, password}).then(res => {
+    Axios.post("/api/login", { username, password }).then(res => {
       console.log(res.data);
       this.setState({
         username: "",
         password: ""
-      })
+      });
       this.hideModal();
-    })
+    });
   };
 
-  searchesGame = (event) => {
+  searchesGame = event => {
     console.log(this.state.title);
     event.preventDefault();
     window.location.assign("/search/" + this.state.title);
     // return (
     //   <Link to={"/search/" + this.state.title} />
-    // ) 
+    // )
   };
 
   render() {
     return (
-  
       <div className="w3-top">
         <div className="w3-bar w3-row w3-padding w3-black" id="navBar">
           {/* <div className="w3-col s3"> */}
@@ -85,23 +80,23 @@ class Nav extends Component {
           {/* </div> */}
           {/* <div className="w3-col s3" id="navSearch"> */}
           <form onSubmit={this.searchesGame}>
-          <Input
-            value={this.state.title}
-            onChange={this.handleInputChange}
-            name="title"
-            placeholder="Game"
-            className="w3-bar-item w3-mobile w3-round-large"
-          />
-          {/* </div> */}
-          {/* <div className="w3-col s3"> */}
-          <FormBtn
-            type="submit"
-            className="w3-bar-item w3-mobile"
-            style={{ marginLeft: 10 }}
-            onClick={this.searchesGame}
-          >
-            Search
-          </FormBtn>
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="Game"
+              className="w3-bar-item w3-mobile w3-round-large"
+            />
+            {/* </div> */}
+            {/* <div className="w3-col s3"> */}
+            <FormBtn
+              type="submit"
+              className="w3-bar-item w3-mobile"
+              style={{ marginLeft: 10 }}
+              onClick={this.searchesGame}
+            >
+              Search
+            </FormBtn>
           </form>
           {/* </div> */}
           {/* <div className="w3-col s3 w3-right"> */}
@@ -112,54 +107,55 @@ class Nav extends Component {
           >
             LOGIN
           </a>
-            <Modal show={this.state.show} handleClose={this.hideModal}>
-              <button
-                className="w3-button w3-xlarge w3-hover-red w3-display-topright"
-                onClick={this.hideModal}
-              >
-                X
+          <Modal show={this.state.show} handleClose={this.hideModal}>
+            <button
+              className="w3-button w3-xlarge w3-hover-red w3-display-topright"
+              onClick={this.hideModal}
+            >
+              X
             </button>
-              <br />
-              <br />
-              <label>
-                <b>Username</b>
-              </label>
-              <Input
-                value={this.state.username}
-                onChange={this.handleInputChange}
-                name="username"
-                placeholder="Username"
-              />
-              <br />
-              <br />
-              <label>
-                <b>Password</b>
-              </label>
-              <Input
-                value={this.state.password}
-                onChange={this.handleInputChange}
-                name="password"
-                placeholder="Password"
-                type="password"
-              />
-              <button onClick={this.userLogin}
-                style={{ marginTop: 30 }}
-                className="w3-display-left w3-button w3-teal w3-round-large"
+            <br />
+            <br />
+            <label>
+              <b>Username</b>
+            </label>
+            <Input
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              name="username"
+              placeholder="Username"
+            />
+            <br />
+            <br />
+            <label>
+              <b>Password</b>
+            </label>
+            <Input
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              name="password"
+              placeholder="Password"
+              type="password"
+            />
+            <button
+              onClick={this.userLogin}
+              style={{ marginTop: 30 }}
+              className="w3-display-left w3-button w3-teal w3-round-large"
+            >
+              Login
+            </button>
 
-              >
-                Login
+            <p style={{ marginTop: 30 }}>OR</p>
+            <button
+              onClick={this.userSignUp}
+              style={{ marginTop: 30 }}
+              className="w3-display-right w3-button w3-teal w3-round-large"
+            >
+              Sign Up
             </button>
-
-              <p style={{ marginTop: 30 }}>OR</p>
-              <button onClick={this.userSignUp}
-                style={{ marginTop: 30 }}
-                className="w3-display-right w3-button w3-teal w3-round-large"
-              >
-                Sign Up
-            </button>
-            </Modal>
-          </div>
+          </Modal>
         </div>
+      </div>
     );
   }
 }
