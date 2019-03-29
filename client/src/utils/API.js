091d1f9c8ec1c2a775c6a464c801e46f4bb28f1c
scc85 <sarahchristinec85@gmail.com>
2019-03-29 11:15:18 -0700

    S

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -19,16 +19,12 @@ export default {
 
 }
 // IDGB API credentials
-// const user_key = "940917f24ab11ddaece60ec17ad01354";
+// const user-key = "940917f24ab11ddaece60ec17ad01354";
 // const IDBM = axios.create({
 //   accept: "application/json",
-//   headers: {"User-Key": user_key}
+//   headers: {"User-Key": user-key}
 // });
-// IDBM rating api
-// IDBM.get("https://api-v3.igdb.com/age_ratings/?fields=*").then(function(res){
-//   console.log(res.data[0].rating);
-// });
-// // IDBM game name/summary api
+// // IDBM game summary/total_rating api
 // IDBM.get("https://api-v3.igdb.com/games/?search=Halo&fields=name,summary").then(function(res){
 //   console.log(res.data[0]);
 // });
